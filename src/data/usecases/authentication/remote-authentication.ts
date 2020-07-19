2cbe8bc99ed2d265e42c30d21cc6d9c92e124182
Erick Malta <erickmalta100@gmail.com>
2020-07-19 20:19:57 -0300

    feat: :ok_hand: ensure RemoteAuthentication calls HttpPostClient with correct URL

---
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -0,0 +1,12 @@
+import { HttpPostClient } from 'data/protocols/http/http-post-client';
+
+export class RemoteAuthentication {
+  constructor (
+    private readonly url: string,
+    private readonly httpClient: HttpPostClient
+  ) {}
+
+  async auth (): Promise<void> {
+    await this.httpClient.post(this.url);
+  }
+}
