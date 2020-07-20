00b2ce4da718800a19656f2629d1ef9b5d81675e
Erick Malta <erickmalta100@gmail.com>
2020-07-20 00:50:02 -0300

    refactor: :recycle: Changed imports to use module

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,7 +1,7 @@
 import { RemoteAuthentication } from './remote-authentication';
-import { HttpPostClientSpy } from '../../test/mock-http-client';
+import { HttpPostClientSpy } from '@/data/test/mock-http-client';
 import faker from 'faker';
-import { mockAuthentication } from '../../../domain/test/mock-authentication';
+import { mockAuthentication } from '@/domain/test/mock-authentication';
 
 type SutTypes = {
   sut: RemoteAuthentication
