4fa50844f3a22a3bf85dd443351627d54d176cd0
Erick Malta <erickmalta100@gmail.com>
2020-07-22 01:35:15 -0300

    refactor: :recycle: Move axios mock to a separate file

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -1,40 +1,38 @@
-import { AxiosHttpClient } from './axios-http-client';
 import axios from 'axios';
-import faker from 'faker';
-import { HttpPostParams } from '@/data/protocols/http';
+import { AxiosHttpClient } from './axios-http-client';
+import { mockAxios } from '@/infra/test';
+import { mockPostRequest } from '@/data/test';
 
 jest.mock('axios');
-const mockedAxios = axios as jest.Mocked<typeof axios>;
-const mockedAxiosResult = {
-  data: faker.random.objectElement(),
-  status: faker.random.number
-};
-mockedAxios.post.mockResolvedValue(mockedAxiosResult);
 
-const makeSut = (): AxiosHttpClient => {
+type SutTypes = {
+  sut: AxiosHttpClient
+  mockedAxios: jest.Mocked<typeof axios>
+};
+
+const makeSut = (): SutTypes => {
   const sut = new AxiosHttpClient();
-  return sut;
+  const mockedAxios = mockAxios();
+  return { sut, mockedAxios };
 };
-
-const mockPostRequest = (): HttpPostParams<any> => ({
-  url: faker.internet.url(),
-  body: faker.random.objectElement()
-});
 
 describe('AxiosHttpClient', () => {
   it('Should call Axios with correct values', async () => {
     const request = mockPostRequest();
-    const sut = makeSut();
+    const { sut, mockedAxios } = makeSut();
     await sut.post(request);
     expect(mockedAxios.post).toHaveBeenCalledWith(request.url, request.body);
   });
 
-  it('Should return the correct statusCode and body', async () => {
-    const sut = makeSut();
-    const httpResponse = await sut.post(mockPostRequest());
-    expect(httpResponse).toEqual({
-      statusCode: mockedAxiosResult.status,
-      body: mockedAxiosResult.data
-    });
+  it('Should return the correct statusCode and body', () => {
+    const { sut, mockedAxios } = makeSut();
+    const promise = sut.post(mockPostRequest());
+    // Pegamos aqui o mockresolvedValue inserido no post
+    // Se fosse o rejected value, seria o índice 1
+    // Pelo mockedresolvedValue retornar uma promise, se compararmos
+    // O valor de forma síncrona, ele irá dar erro, por isso
+    // Podemos ao invés disso, capturar a promise do post, sem o await
+    // E comparar essa promise com a promise retornada pelo mockedresolvedValue
+    expect(promise).toEqual(mockedAxios.post.mock.results[0].value);
   });
 });
