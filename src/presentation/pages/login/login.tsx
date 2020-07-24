d1a1d2fcd1ca809d4f45f3440f3b4b7af833098e
Erick Malta <erickmalta100@gmail.com>
2020-07-24 01:31:09 -0300

    refactor: :recycle: Moved form status to a separate file

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import Styles from './login-styles.scss';
-import Spinner from '@/presentation/components/spinner/spinner';
 import Header from '@/presentation/components/login-header/login-header';
 import Footer from '@/presentation/components/footer/footer';
 import Input from '@/presentation/components/input/input';
+import FormStatus from '@/presentation/components/form-status/form-status';
 
 const Login: React.FC = () => {
   return (
@@ -17,10 +17,7 @@ const Login: React.FC = () => {
           Entrar
         </button>
         <span className={Styles.link}>Criar conta</span>
-        <div className={Styles.errorWrap}>
-          <Spinner className={Styles.spinner} />
-          <span className={Styles.error}>Erro</span>
-        </div>
+        <FormStatus />
       </form>
       <Footer />
     </div>
