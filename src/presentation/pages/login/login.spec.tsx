d632f8a33831dbac54ccff64c3266dc4a6d08d5f
Erick Malta <erickmalta100@gmail.com>
2020-07-26 16:16:05 -0300

    test: :construction: Ensure Login calls Validation with correct password

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -56,4 +56,13 @@ describe('Login Component', () => {
       email: 'any_email'
     });
   });
+
+  it('Should call Validation with correct password', () => {
+    const { sut, validationSpy } = makeSut();
+    const passwordInput = sut.getByTestId('password');
+    fireEvent.input(passwordInput, { target: { value: 'any_password' } });
+    expect(validationSpy.input).toEqual({
+      password: 'any_password'
+    });
+  });
 });
