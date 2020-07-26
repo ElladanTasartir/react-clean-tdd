763b7d6ee0abf83d0e4b38ab8f7d9399447bd910
Erick Malta <erickmalta100@gmail.com>
2020-07-26 15:46:06 -0300

    refactor: :recycle: Move SUT creation to a factory method

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,20 +1,29 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, RenderResult } from '@testing-library/react';
 import Login from './login';
+
+type SutTypes = {
+  sut: RenderResult
+};
+
+const makeSut = (): SutTypes => {
+  const sut = render(<Login />);
+  return { sut };
+};
 
 describe('Login Component', () => {
   it('Should start with initial state', () => {
-    const { getByTestId } = render(<Login />);
+    const { sut } = makeSut();
     // Caso, o elemento que tentamos pegar em tela não consiga ser pego
     // Ele crasha o teste, ao invés de falhar
-    const errorWrap = getByTestId('error-wrap');
+    const errorWrap = sut.getByTestId('error-wrap');
     expect(errorWrap.childElementCount).toBe(0);
-    const submitButton = getByTestId('submit') as HTMLButtonElement;
+    const submitButton = sut.getByTestId('submit') as HTMLButtonElement;
     expect(submitButton.disabled).toBe(true);
-    const emailStatus = getByTestId('email-status');
+    const emailStatus = sut.getByTestId('email-status');
     expect(emailStatus.title).toBe('Campo obrigatório');
     expect(emailStatus.textContent).toBe('🔴');
-    const passwordStatus = getByTestId('password-status');
+    const passwordStatus = sut.getByTestId('password-status');
     expect(passwordStatus.title).toBe('Campo obrigatório');
     expect(passwordStatus.textContent).toBe('🔴');
   });
