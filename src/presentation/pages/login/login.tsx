0fc3629c01116f67f0fce7deb86e76f605fdb1c2
Erick Malta <erickmalta100@gmail.com>
2020-07-25 23:29:01 -0300

    feat: :ok_hand: Ensure Login starts with submit button disabled

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -28,7 +28,12 @@ const Login: React.FC = () => {
             name="password"
             placeholder="Digite sua senha"
           />
-          <button className={Styles.submit} type="submit">
+          <button
+            data-testid="submit"
+            disabled
+            className={Styles.submit}
+            type="submit"
+          >
             Entrar
           </button>
           <span className={Styles.link}>Criar conta</span>
