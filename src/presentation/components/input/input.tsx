a11210e92fcfda427a3207b1853b63d4ceb99fdd
Erick Malta <erickmalta100@gmail.com>
2020-07-26 22:13:48 -0300

    feat: :ok_hand: Ensure Login presents valid state if password is valid

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -22,11 +22,11 @@ const Input: React.FC<Props> = (props: Props) => {
   };
 
   const getStatus = (): string => {
-    return '🔴';
+    return error ? '🔴' : '🟢';
   };
 
   const getTitle = (): string => {
-    return error;
+    return error || 'Tudo certo!';
   };
 
   return (
